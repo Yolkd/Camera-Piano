55ba378731fe6edf4c18f7d37abc62b431d5c7d6
Yolkd <thinhtruong1421@gmail.com>
2024-09-28 15:17:01 -0400

    Update keyPress.py

--- keyPress.py
+++ keyPress.py
@@ -4,7 +4,7 @@ from cvzone.HandTrackingModule import HandDetector
 detector = HandDetector(detectionCon=0.8)
 
 class Button():
-    def __init__(self, pos, text, size=[100,100]):
+    def __init__(self, pos, text, size=[50,20]):
         self.pos = pos
         self.size = size
         self.text = text
@@ -12,7 +12,7 @@ class Button():
     def keyCreate(self, img):
         x, y = self.pos
         w, h = self.size
-        cv2.rectangle(img, self.pos, (x+w, y+h), (255, 0, 255), cv2.FILLED)
-        cv2.putText(img, self.text, (x + 25, y + 25), cv2.FONT_HERSHEY_PLAIN, 5, (255, 255, 255), 5)
+        cv2.rectangle(img, self.pos, (x+w, y+h), (255, 255, 255), cv2.FILLED)
+        # cv2.putText(img, self.text, (x + 50, y + 50), cv2.FONT_HERSHEY_PLAIN, 5, (0, 0, 0), 5)
 
         return img
